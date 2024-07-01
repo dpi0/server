be8eeb584fe67b928591c81c7b51e1cd53e9f2da
dpi0 <dpi0.dev@proton.me>
2024-07-01 19:43:17 +0530

    improved arch bootstrap

--- arch.sh
+++ arch.sh
@@ -3,25 +3,23 @@
 SERVER="$HOME/.server"
 CONFIG="$HOME/.config"
 
-sudo pacman -Syu
+sudo pacman -Syu --noconfirm
+sudo pacman -Sy --noconfirm zsh tmux fzf neovim bat exa zoxide ripgrep fd btop nftables docker docker-compose
 
-sudo pacman -Sy zsh tmux fzf neovim bat eza zoxide ripgrep fd btop nftables
-sudo pacman -Sy docker docker-compose
 sudo usermod -aG docker $USER
-sudo systemctl enable docker.service
-sudo systemctl enable containerd.service
-sudo systemctl start docker.service
+sudo systemctl enable --now docker.service containerd.service
 
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 
-mkdir ~/.config
+mkdir -p "$CONFIG"
 
-rm -f $HOME/.zshrc; ln -s $SERVER/zsh/.zshrc $HOME/.zshrc
-ln -s "$SERVER/tmux/" "$CONFIG/"
+ln -sf "$SERVER/zsh/.zshrc" "$HOME/.zshrc"
+ln -sf "$SERVER/tmux/" "$CONFIG/"
 
 tmux kill-server
 sudo chsh -s $(which zsh) $USER
-zsh
+
+exec zsh
 
 # sudo reboot
