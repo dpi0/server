23280a527ecec6935de106f146eb95a0ecf88c5f
dpi0 <dpi0.dev@proton.me>
2024-07-08 13:00:49 +0530

    Update minimal-ubuntu.sh

--- minimal-ubuntu.sh
+++ minimal-ubuntu.sh
@@ -3,6 +3,23 @@
 SERVER="$HOME/.server"
 CONFIG="$HOME/.config"
 
+sudo hostnamectl set-hostname wsl
+logout
+
+date
+timedatectl status
+sudo apt install util-linux-extra (needed in 24.04, not in the ones before it)
+timedatectl set-timezone "Asia/Kolkata"
+sudo ln -sf /usr/share/zoneinfo/Asia/Kolkata /etc/localtime
+sudo hwclock --systohc
+date
+
+
+# get a list of packages installed with date
+zgrep 'install ' /var/log/dpkg.log* | sort | cut -f1,2,4 -d' '
+# OR
+tail /var/log/dpkg.log
+
 sudo apt update && sudo apt upgrade -y
 sudo apt install zsh tmux fzf neovim bat ripgrep fd-find btop -y
 sudo apt install eza -y # only on 24.04
